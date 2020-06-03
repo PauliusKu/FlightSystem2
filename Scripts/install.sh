2362546694558a6df284b3fc9bcf0382a84617f5
Paulius <paulius.kuprevicius@gmail.com>
2020-06-03 19:04:16 +0300

    dgjh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -2,7 +2,7 @@
 
 sudo rm -rf /src/Rebus/*
 
-sudo yum install docker
+sudo yes | yum install docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
