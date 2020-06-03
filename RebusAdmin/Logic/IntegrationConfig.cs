07651ff7ddebb0990f00a4fe8edad4e9c75a584b
Paulius <paulius.kuprevicius@gmail.com>
2020-06-04 00:14:02 +0300

    sdfgsgd

--- RebusAdmin/Logic/IntegrationConfig.cs
+++ RebusAdmin/Logic/IntegrationConfig.cs
@@ -3,12 +3,12 @@
     public static class IntegrationConfig
     {
 
-        public static string GetDataBaseUri() { return OperatingSys.IsLinux() ? "bolt://neo4j:7687" : "bolt://localhost:7687"; }
+        public static string GetDataBaseUri() { return OperatingSys.IsLinux() ? "bolt://54.175.189.247:7687" : "bolt://localhost:7687"; }
 
         public static string GetRebusNeoUri() { return "http://rebusneo:5002/"; }
 
         public static string GetDataBaseUsername() { return "neo4j"; }
-        public static string GetDataBasePassword() { return "123"; }
+        public static string GetDataBasePassword() { return "tzTBavvjGBm0"; }
 
         public static string GetDataLocation() { return OperatingSys.IsLinux() ? "/app/files/" : @"C:\Users\pkuprevicius\Documents\dotnet\FlightSystem2\RebusAdmin\Files\"; }
     }
